9e5ca7fd3a05b49df657c15caccd5b6cff8f9c22
Khrystenko_Denys <deonis062002@gmail.com>
2024-04-04 14:28:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+from os import path
+
+from app.customer import Customer
+
+
+def shop_trip() -> None:
+    with open(path.join("app", "config.json"), "r") as file:
+        info = json.load(file)
+
+    fuel_info = info["FUEL_PRICE"]
+    customers_info = info["customers"]
+    shop_info = info["shops"]
+    if customers_info:
+        for customer in customers_info:
+            Customer(customer).customer_money()
+            Customer(customer).cheaper_shop(shop_info, fuel_info)
